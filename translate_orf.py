3d9bfda247fb4b7ad146d4ba89fc6a50d070a431
Chloe Kaczvinsky <cak0052@tigermail.auburn.edu>
2019-04-03 09:33:20 -0500

    Added translate functionality and deleted duplicated input options

--- translate_orf.py
+++ translate_orf.py
@@ -1,7 +1,7 @@
 import sys
 import re
 import find_orf 
-
+import translate
 
 def main():
     import argparse
@@ -43,20 +43,9 @@ def main():
     else:
         sequence = args.sequence
 
-    orf = find_first_orf(sequence = sequence,
+    orf = find_orf.find_first_orf(sequence = sequence,
             start_codons = args.start_codons,
             stop_codons = args.stop_codons)
-    parser.add_argument('-s', '--start-codons',
-            type = str,
-            nargs = '+', # one or more arguments
-            default = ['AUG'],
-            help = ('One or more possible start codons.'))
-    parser.add_argument('-x', '--stop-codons',
-            type = str,
-            nargs = '+', # one or more arguments
-            default = ['UAA', 'UAG', 'UGA'],
-            help = ('One or more possible stop codons.'))
-
     # Parse the command-line arguments into a 'dict'-like container
     args = parser.parse_args()
 
@@ -67,14 +56,15 @@ def main():
     else:
         sequence = args.sequence
 
-    orf = find_first_orf(sequence = sequence,
+    orf = find_orf.find_first_orf(sequence = sequence,
             start_codons = args.start_codons,
             stop_codons = args.stop_codons)
-    sys.stdout.write('{}\n'.format(orf))
+    pep = translate.get_longest_peptide(orf, genetic_code)
+    sys.stdout.write('{}\n'.format(pep))
 
 
 if __name__ == '__main__':
  genetic_code = {'GUC': 'V', 'ACC': 'T', 'GUA': 'V', 'GUG': 'V', 'ACU': 'T', 'AAC': 'N', 'CCU': 'P', 'UGG': 'W', 'AGC': 'S', 'AUC': 'I', 'CAU': 'H', 'AAU': 'N', 'AGU': 'S', 'GUU': 'V', 'CAC': 'H', 'ACG': 'T', 'CCG': 'P', 'CCA': 'P', 'ACA': 'T', 'CCC': 'P', 'UGU': 'C', 'GGU': 'G', 'UCU': 'S', 'GCG': 'A', 'UGC': 'C', 'CAG': 'Q', 'GAU': 'D', 'UAU': 'Y', 'CGG': 'R', 'UCG': 'S', 'AGG': 'R', 'GGG': 'G', 'UCC': 'S', 'UCA': 'S', 'UAA': '*', 'GGA': 'G', 'UAC': 'Y', 'GAC': 'D', 'UAG': '*', 'AUA': 'I', 'GCA': 'A', 'CUU': 'L', 'GGC': 'G', 'AUG': 'M', 'CUG': 'L', 'GAG': 'E', 'CUC': 'L', 'AGA': 'R', 'CUA': 'L', 'GCC': 'A', 'AAA': 'K', 'AAG': 'K', 'CAA': 'Q', 'UUU': 'F', 'CGU': 'R', 'CGC': 'R', 'CGA': 'R', 'GCU': 'A', 'GAA': 'E', 'AUU': 'I', 'UUG': 'L', 'UUA': 'L', 'UGA': '*', 'UUC': 'F'}
  main()
-
+ 
 
